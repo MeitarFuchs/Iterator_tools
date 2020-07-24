bc11f9b915b5b7643722bea82ff17a573977735b
MeitarFuchs <meitardar2@gmail.com>
2020-07-24 13:52:56 +0300

    fix acu

--- accumulate.hpp
+++ accumulate.hpp
@@ -15,7 +15,7 @@ namespace itertools {
 
     template<class T,class FUNC = add>
     class accumulate {
-         const  T& container;
+        const  T& container;
         const FUNC& function;
     public:
 
@@ -23,54 +23,47 @@ namespace itertools {
 
         class iterator{
             decltype(container.begin()) iter;
-            typename std::decay<decltype(*(container.begin()))>::type sum;
+            typename std::decay<decltype(*(container.begin()))>::type sum;// to remove the const..
             const accumulate& acc;
 
 
         public:
             iterator(decltype(container.begin()) curr,const accumulate& acc): iter(curr),acc(acc) {
-                if (curr != acc.container.end())sum = *curr;
+                if (curr!= acc.container.end())sum = *curr;
             }
-
-            iterator& operator++(){  //++iter
+            iterator& operator++(){  //++i
                 ++iter;
                 if(iter==acc.container.end())return *this;
                 sum=acc.function(sum,*iter);
                 return *this;
             }
-
-            const iterator operator++(int){ //iter++
+            const iterator operator++(int){ //i++
                 iterator temp = *this;
                 sum=acc.function(sum,*(iter++));
                 return temp;
             }
-
-            bool operator==(const iterator &it) const {
+            bool operator==(const iterator &it) const {// we just comper no change anything
                 return iter==it.iter;
             }
-
-            bool operator!=(const iterator &it) const {
+            bool operator!=(const iterator &it) const {// we just comper no change anything
                 return !(iter==it.iter);
             }
-
             auto operator*(){
                 return sum;
             }
-
-            iterator& operator=(const iterator &temp_iter)
-            {
-                return *this;
-            }
+//            iterator& operator=(const iterator &temp_iter) // we just comper no change anything
+//            {
+//                return *this;
+//            }
 
         };
 
-        iterator begin() const {
+        iterator begin() const { //the const is on the this
             return iterator(container.begin(),*this);
 
         }
-        iterator end() const {
+        iterator end() const { //the const is on the this
             return iterator(container.end(),*this);
         }
     };
-
 }
